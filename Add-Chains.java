dc3c0fbf1bbd42b34cef3bcb38f9ac708280abf0
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 10:44:26 +0100

    Create Add-Chains.java

---
+++ Add-Chains.java
@@ -0,0 +1,8 @@
+public Class Main{
+    public static void main(String[] args){
+        String firstName = "John";
+        String lastName = "Rambo";
+        
+        System.out.println("Where are the missiles, " + firstName + " " + lastName + " ?");
+    }
+}
