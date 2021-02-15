89dc79370c57a4461980c5762f2362e88536869e
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 10:59:41 +0100

    Create Booleans.java

---
+++ Booleans.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        boolean isRamboStrong = true;
+        boolean isElonDumb = false;
+        
+        System.out.println(isRamboStrong);
+        System.out.println(isElonDumb);
+    }
+}
