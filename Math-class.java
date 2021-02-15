eea29e31ae7e9c9560ca71435b33639bab991326
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 11:40:30 +0100

    Create Math-class.java

---
+++ Math-class.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+    int a = 5;
+    int b = 7;
+    float c = -3.47f;
+    
+    // Trouver le plus grand nombre entre deux variables
+    System.out.println(Math.max(a, b));
+    
+    // Trouver le plus petit nombre entre deux variables
+    System.out.println(Math.min(a, b));
+    
+    // Trouver la racine carrée d'une valeur
+    System.out.println(Math.sqrt(a));
+    
+    // Trouver la valeur positive absolue d'une valeur
+    System.out.println(Math.abs(c));
+    
+    // Afficher un nombre aléatoire
+    System.out.println(Math.random());
+    }   
+}
