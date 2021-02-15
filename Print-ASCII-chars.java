af52a1b59ead20dcd4c300c34b5893efa0dbccf0
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 11:04:05 +0100

    Create Print-ASCII-chars.java

---
+++ Print-ASCII-chars.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+         char a = 65;
+         char b = 66;
+         char c = 67;
+         
+         System.out.println("The three first letters of the alphabet are " + a + ", " + b + ", " + c + ".");
+    }
+}
