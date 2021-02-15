df2e792c83cd93a6832e7aff9d6d5705e43a15ee
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 10:50:16 +0100

    Create Add-Variables.java

---
+++ Add-Variables.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 4;
+        int y = 6;
+        
+        System.out.println("x + y equals " + (x + y) + ".");
+    }
+}
