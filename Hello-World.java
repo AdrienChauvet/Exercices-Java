929c2bbffd79ce7f17c2ff5c098df99801caadb4
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-15 10:37:44 +0100

    Update Hello-World.java

--- Hello-World.java
+++ Hello-World.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String[] args){
+        // This is a comment.
+        /*
+          This is a multiple lignes comment.
+        */
         System.out.println("Hello, world!");
     }
 }
