5cdecd27edcb373d4b0dc408522b51477916828e
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-17 16:14:40 +0100

    Create ExoTableaux2.java

---
+++ ExoTableaux2.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        String[] tab = new String[] {"a", "e", "i", "o", "u", "y"};
+        
+        for (int i = 0 ; i < tab.length ; i++) {
+            System.out.println(tab[i]);
+        }
+    }
+}
