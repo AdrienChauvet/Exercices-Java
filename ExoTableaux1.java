fbf4c0d3550e3dd75fa43dc835351b8159a8ee0a
Adrien Chauvet <23014172+AdrienChauvet@users.noreply.github.com>
2021-02-17 13:44:47 +0100

    Create ExoTableaux1.java

---
+++ ExoTableaux1.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int[] tab = new int[7];
+
+        for (int i = 0 ; i < tab.length ; i++) {
+            tab[i] = 0;
+            System.out.println(tab[i]);
+        }
+    }
+}
